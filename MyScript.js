f91a04af60b338e174e0b0e3dad0dfec6f713e3c
Prazial <161205735+Prazial@users.noreply.github.com>
2025-05-29 05:19:46 +0000

    EDITING

--- MyScript.js
+++ MyScript.js
@@ -1,13 +1,11 @@
 let playerchoice = ["rock", "paper", "scissors"];
-function getcomputerchioice(){
+function getcomputerchoice(){
     return playerchoice[Math.floor(Math.random() * playerchoice.length)];
 }
-console.log(getcomputerchioice());
+console.log(getcomputerchoice());
 
 function gethumanchoice(){
     let choice = prompt("Enter your choice (rock, paper, scissors):");
-    if (!playerchoice.includes(choice)) {
-        console.log("Invalid choice. Please enter rock, paper, or scissors.");
-        return gethumanchoice(); // Recursively prompt until a valid choice is made
+    return choice; // Recursively prompt until a valid choice is made
     }
 console.log(gethumanchoice());
