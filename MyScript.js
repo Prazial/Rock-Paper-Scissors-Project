5784b4b881774ec39c5d87a9e191d1cad38cd18e
Prazial <161205735+Prazial@users.noreply.github.com>
2025-05-30 05:11:35 +0000

    Edit

--- MyScript.js
+++ MyScript.js
@@ -24,8 +24,7 @@ function playRound(humanchoice,computerchoice) {
 
 function gethumanchoice(){
     let input = prompt("Enter your choice (rock, paper, scissors):");
-    input = input.toLowerCase();
-    // Convert input to lowercase to handle case insensitivity
+    return input; // Convert input to lowercase to handle case insensitivity
 }console.log(`You chose: ${humanSelection}`);
 
 
